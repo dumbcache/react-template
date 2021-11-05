94f12a3ab4097adc934d91f5750d61e336432576
yesu4658 <yesubabu.gudi@gmail.com>
2021-11-05 20:48:15 +0530

    support for assets

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,25 @@ module.exports = {
                     loader: "babel-loader",
                 },
             },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
         ],
     },
     resolve: {
@@ -23,6 +42,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
+            title: "Output Management",
             template: "./src/index.html",
         }),
     ],
